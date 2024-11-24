d18fb1bf38359c0c08e5d61220c14a8896ae6d4e
Jessica Gondim <jessicagbsg@gmail.com>
2024-11-24 17:59:05 -0300

    feat: add trash to note

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -1,32 +1,36 @@
 import { cn } from "@/lib/utils";
+import { Trash } from "lucide-react";
 
 export const Note = ({ id, title, todos }: Props) => {
   return (
     <div
       key={`note-${id}`}
       onClick={() => console.log("clicked")}
-      className={`flex flex-col items-start min-w-44 w-full max-w-full sm:max-w-56
-        h-44 gap-y-2 p-4 cursor-pointer bg-secondary/10 rounded-lg shadow-md`}
+      className={`flex flex-col min-w-44 w-full max-w-full sm:max-w-56 h-44 
+         gap-y-2 p-4 cursor-pointer bg-secondary/10 rounded-lg shadow-md`}
     >
       <h4 className="text-base font-semibold text-primary line-clamp-1">{title}</h4>
+      <div className="flex flex-col mt-2 h-full justify-between">
+        <div className="flex flex-col gap-y-1 w-full">
+          {todos.map((todo, index) => (
+            <div
+              key={`todo-${index}`}
+              className={cn(
+                "flex items-center gap-x-2",
+                todo.done && "line-through text-muted-foreground opacity-50"
+              )}
+            >
+              <input
+                type="checkbox"
+                checked={todo.done}
+                onChange={(e) => console.log(e.target.checked)}
+              />
+              <p className="text-sm text-primary line-clamp-1">{todo.text}</p>
+            </div>
+          ))}
+        </div>
 
-      <div className="flex flex-col gap-y-1 w-full">
-        {todos.map((todo, index) => (
-          <div
-            key={`todo-${index}`}
-            className={cn(
-              "flex items-center gap-x-2",
-              todo.done && "line-through text-muted-foreground opacity-50"
-            )}
-          >
-            <input
-              type="checkbox"
-              checked={todo.done}
-              onChange={(e) => console.log(e.target.checked)}
-            />
-            <p className="text-sm text-primary line-clamp-1">{todo.text}</p>
-          </div>
-        ))}
+        <Trash className="self-end h-4 w-4 text-muted-foreground" />
       </div>
     </div>
   );
